9afaed1ba9e0f5272ac43cc6fa9eae045bfe7592
Rainch <2864080974@qq.com>
2023-01-24 17:40:33 +0800

    third commit

--- src/main/java/com/rainch/App.java
+++ src/main/java/com/rainch/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Git!" );
         System.out.println( "Hello Git!second" );
+        System.out.println( "Hello Git!third" );
         System.out.println("你好，Git！");
     }
 }
