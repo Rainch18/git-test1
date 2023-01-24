7857272a262301b252684c36ac7a185454ec2828
Rainch <2864080974@qq.com>
2023-01-24 17:40:02 +0800

    second commit

--- src/main/java/com/rainch/App.java
+++ src/main/java/com/rainch/App.java
@@ -7,6 +7,7 @@ package com.rainch;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Git!" );
+        System.out.println( "Hello Git!second" );
         System.out.println("你好，Git！");
     }
 }
