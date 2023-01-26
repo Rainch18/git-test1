27fb7a8c3dd9406aa16e5a257827d237ac61ff57
Rainch <2864080974@qq.com>
2023-01-26 17:41:41 +0800

    github commit test

--- src/main/java/com/rainch/GitTest.java
+++ src/main/java/com/rainch/GitTest.java
@@ -1,7 +1,13 @@
 package com.rainch;
 
+import java.util.Scanner;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git真好玩!");
+        System.out.println("Github上传代码");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i);
     }
 }
