3ceb350b1a65e3b518ef249a407587a193fc7df7
Rainch <85862201+Rainch18@users.noreply.github.com>
2023-01-26 16:48:45 +0800

    Update App.java

--- src/main/java/com/rainch/App.java
+++ src/main/java/com/rainch/App.java
@@ -13,6 +13,7 @@ public class App {
         System.out.println( "Hello Git master test" );
         System.out.println( "Hello Git hot-fix test!" );
         System.out.println( "Git push" );
+        System.out.println( "Git pull" );
         System.out.println("你好，Git！");
     }
 }
